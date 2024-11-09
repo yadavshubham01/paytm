311b95ea92e27496096619a1e5c2d88241fdb25d
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-11-09 19:18:41 +0530

    update balance value

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,29 @@
+import { useState ,useEffect } from "react";
 import { Appbar } from "../components/Appbar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/User";
-
+import axios from "axios";
 
 export function Dashboard(){
+    const [balance, setBalance] = useState(null);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}` // Add auth header if needed
+            }
+        })
+        .then(response => {
+            setBalance(response.data.balance); // Set balance from response data
+        })
+        .catch(error => {
+            console.error('Error fetching balance:', error);
+        });
+    }, []);
     return <div>
         <Appbar/>
         <div className="m-8">
-            <Balance value={"10,000"}/>
+            <Balance value={balance}/>
             <Users/>
         </div>
     </div>
